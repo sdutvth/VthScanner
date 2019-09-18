a566473a61d557658bb6ac6a0b2d25522c9a8663
sdutvth <vth1311@126.com>
2019-09-18 22:29:00 +0800

    完成了爆破工具编写

--- BruteForce/Linux/get_password_by_users.py
+++ BruteForce/Linux/get_password_by_users.py
@@ -1,6 +1,6 @@
 import re
 import crypt
-def get_users(file_ptr):
+def get_users(file_ptr): # 拿到所有的用户名、盐值、加密后的值等数据
 	data_lines = file_ptr.readlines()
 	user_salts_results = []
 	for data_line in data_lines:
@@ -11,7 +11,7 @@ def get_users(file_ptr):
 			user_salts_results.append((user,salt,res))
 	return user_salts_results
 
-def UI(user_salts_results):
+def UI(user_salts_results): # UI交互
 	users = []
 	for data in user_salts_results:
 		users.append(data[0])
@@ -25,5 +25,15 @@ def UI(user_salts_results):
 def main():
 	file_ptr = open('./HackedFiles/shadow','r')
 	user_salts_results = get_users(file_ptr)
-	targets_user = UI(user_salts_results)
+	targets_user,target_salt,target_encode = UI(user_salts_results)
+	fp_passwd = open('password.txt','r')
+	passwords = fp_passwd.readlines()
+	fp_passwd.close()
+	for i in passwords:
+		encoded = crypt.crypt(i[:-1],target_salt) # 注意字符串末尾是\n,要过滤掉
+		if encoded == target_encode:
+			print('user %s\'s password is %s' %(targets_user,i))
+			break
+	else:
+		print('sorry,not found!')
 main()
